17a7f872317a6e61c27779ce5503710ca35faaaa
Nathvp <129067275+Nathvp@users.noreply.github.com>
2024-06-17 18:47:44 -0300

    .

--- script.js
+++ script.js
@@ -81,8 +81,13 @@ let perguntaAtual;
 let historiaFinal = "";
 
 function mostraPergunta() {
+    if(atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
 
@@ -97,8 +102,15 @@ function mostraAlternativas(){
 
 function respostaSelecionada(opcaoSelecionada){
         const afirmacoes = opcaoSelecionada.afirmacoes;
-        historiaFinal = afirmacoes;
+        historiaFinal += afirmacoes + " ";
         atual++;
         mostraPergunta();
 }
+
+function mostraResultado(){
+     caixaPerguntas.textContent = "Em 2049...";
+     textoResultado.textContent = historiaFinal;
+     caixaAlternativas.textContent = "";
+}
+
 mostraPergunta();
