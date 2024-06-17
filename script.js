c02a15b99c443217bb3ac4bc48ff8ff5c84138e7
Nathvp <129067275+Nathvp@users.noreply.github.com>
2024-06-17 11:44:47 -0300

    .

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
