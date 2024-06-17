ab51d3296fdd829637d2259b674da8a75c8bc03d
Nathvp <129067275+Nathvp@users.noreply.github.com>
2024-06-17 17:46:26 -0300

    aula 4

--- script.js
+++ script.js
@@ -3,3 +3,42 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+    enunciado: "Pergunta 1",
+    alternativas: [
+        "Alternativa 1", 
+        "Alternativa 2"
+    ] 
+},
+{
+    enunciado: "Pergunta 2",
+    alternativas: [
+        "Alternativa 1", 
+        "Alternativa 2"
+    ] 
+},
+{
+    enunciado: "Pergunta 3",
+    alternativas: [
+        "Alternativa 1", 
+        "Alternativa 2"
+    ] 
+},
+{
+    enunciado: "Pergunta 4",
+    alternativas: [
+        "Alternativa 1", 
+        "Alternativa 2"
+    ] 
+},
+{
+    enunciado: "Pergunta 5",
+    alternativas: [
+        "Alternativa 1", 
+        "Alternativa 2"
+    ] 
+},
+    
+];
